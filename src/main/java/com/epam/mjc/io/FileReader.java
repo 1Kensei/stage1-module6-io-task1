30cede5daab48c5261218dfea0508d7da6958220
diasbuz <grandleocraft@gmail.com>
2025-07-19 17:46:37 +0600

    Implemented Profile object parsing1.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -20,11 +20,11 @@ public class FileReader {
             while ((b = inputStream.read()) != -1) {
                 stringBuilder.append((char) b);
             }
-            String[] vals = stringBuilder.toString().trim().split("\r\n");
-            name = vals[0].substring(vals[0].indexOf(':') + 1).trim();
-            age = vals[1].substring(vals[1].indexOf(':') + 1).trim();
-            email = vals[2].substring(vals[2].indexOf(':') + 1).trim();
-            phone = vals[3].substring(vals[3].indexOf(':') + 1).trim();
+            String[] vals = stringBuilder.toString().trim().split("\r?\n");
+            name = vals.length > 0 ? vals[0].substring(vals[0].indexOf(':') + 1).trim() : "";
+            age = vals.length > 1 ? vals[1].substring(vals[1].indexOf(':') + 1).trim() : "";
+            email = vals.length > 2 ? vals[2].substring(vals[2].indexOf(':') + 1).trim() : "";
+            phone = vals.length > 3 ? vals[3].substring(vals[3].indexOf(':') + 1).trim() : "";
 
             return new Profile(name, Integer.parseInt(age), email, Long.parseLong(phone));
 
