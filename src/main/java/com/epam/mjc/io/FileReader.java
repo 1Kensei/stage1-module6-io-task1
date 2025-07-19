3a8c81d50b30a31e38fba2bca0f3a1ee6fd1fe7f
diasbuz <grandleocraft@gmail.com>
2025-07-19 17:35:17 +0600

    Implemented Profile object parsing.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,37 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name;
+        String age;
+        String email;
+        String phone;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            int b;
+            while ((b = inputStream.read()) != -1) {
+                stringBuilder.append((char) b);
+            }
+            String[] vals = stringBuilder.toString().trim().split("\r\n");
+            name = vals[0].substring(vals[0].indexOf(':') + 1).trim();
+            age = vals[1].substring(vals[1].indexOf(':') + 1).trim();
+            email = vals[2].substring(vals[2].indexOf(':') + 1).trim();
+            phone = vals[3].substring(vals[3].indexOf(':') + 1).trim();
+
+            return new Profile(name, Integer.parseInt(age), email, Long.parseLong(phone));
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
